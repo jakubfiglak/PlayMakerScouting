a29e08f23afab93381a49b9fce7502f47231954a
Jakub Figlak <jakub.figlak@gmail.com>
2020-08-09 20:02:30 +0200

    Prepare for deploy - add heroku-postbuild script, serve static folder to the server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const colors = require('colors');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const xss = require('xss-clean');
+const path = require('path');
 const connectDB = require('./config/db');
 const errorHandler = require('./middleware/error');
 
@@ -44,6 +45,16 @@ app.use(`${process.env.BASE_URL}/players`, players);
 app.use(`${process.env.BASE_URL}/matches`, matches);
 app.use(`${process.env.BASE_URL}/orders`, orders);
 app.use(`${process.env.BASE_URL}/reports`, reports);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 app.use(errorHandler);
 
