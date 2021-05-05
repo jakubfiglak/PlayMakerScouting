af2b1386ddc1cee97c8e8748c283991ba33e5038
Jakub Figlak <jakub.figlak@gmail.com>
2021-05-05 22:14:18 +0200

    Add loader on ratings and reportTemplates tables

--- client/src/pages/ReportTemplates/ReportTemplatesTable.tsx
+++ client/src/pages/ReportTemplates/ReportTemplatesTable.tsx
@@ -7,6 +7,7 @@ import { Edit as EditIcon, Delete as DeleteIcon } from '@material-ui/icons';
 import { SimpleTable } from '../../components/table/SimpleTable';
 import { StyledTableCell } from '../../components/table/TableCell';
 import { StyledTableRow } from '../../components/table/TableRow';
+import { Loader } from '../../components/Loader';
 // Types
 import { ReportTemplate } from '../../types/reportTemplates';
 // Hooks
@@ -25,46 +26,49 @@ type Props = {
 
 export const ReportTemplatesTable = ({ onEditClick, onDeleteClick }: Props) => {
   const classes = useStyles();
-  const { data } = useReportTemplates();
+  const { data, isLoading } = useReportTemplates();
   const user = useAuthenticatedUser();
 
   return (
-    <SimpleTable actions headCells={headCells}>
-      {data
-        ? data.map((template) => (
-            <StyledTableRow key={template.id}>
-              <StyledTableCell padding="checkbox">
-                <div className={classes.buttonsContainer}>
-                  <Tooltip title="Edytuj">
-                    <IconButton
-                      aria-label="edit"
-                      onClick={() => onEditClick(template)}
-                      disabled={
-                        user.role !== 'admin' && template.author !== user.id
-                      }
-                    >
-                      <EditIcon />
-                    </IconButton>
-                  </Tooltip>
-                  <Tooltip title="Usuń">
-                    <IconButton
-                      aria-label="delete"
-                      onClick={() => onDeleteClick(template)}
-                      disabled={
-                        user.role !== 'admin' && template.author !== user.id
-                      }
-                    >
-                      <DeleteIcon color="error" />
-                    </IconButton>
-                  </Tooltip>
-                </div>
-              </StyledTableCell>
-              <StyledTableCell>{template.name}</StyledTableCell>
-              <StyledTableCell>{template.maxRatingScore}</StyledTableCell>
-            </StyledTableRow>
-          ))
-        : null}
-    </SimpleTable>
+    <>
+      {isLoading && <Loader />}
+      <SimpleTable actions headCells={headCells}>
+        {data
+          ? data.map((template) => (
+              <StyledTableRow key={template.id}>
+                <StyledTableCell padding="checkbox">
+                  <div className={classes.buttonsContainer}>
+                    <Tooltip title="Edytuj">
+                      <IconButton
+                        aria-label="edit"
+                        onClick={() => onEditClick(template)}
+                        disabled={
+                          user.role !== 'admin' && template.author !== user.id
+                        }
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="Usuń">
+                      <IconButton
+                        aria-label="delete"
+                        onClick={() => onDeleteClick(template)}
+                        disabled={
+                          user.role !== 'admin' && template.author !== user.id
+                        }
+                      >
+                        <DeleteIcon color="error" />
+                      </IconButton>
+                    </Tooltip>
+                  </div>
+                </StyledTableCell>
+                <StyledTableCell>{template.name}</StyledTableCell>
+                <StyledTableCell>{template.maxRatingScore}</StyledTableCell>
+              </StyledTableRow>
+            ))
+          : null}
+      </SimpleTable>
+    </>
   );
 };
 
