1612b6ffb152c26dad52a47674df4168793266a1
Jakub Figlak <jakub.figlak@gmail.com>
2021-07-12 22:17:40 +0200

    Fix printing from report table bug

--- client/src/pages/Reports/ReportsTable.tsx
+++ client/src/pages/Reports/ReportsTable.tsx
@@ -65,7 +65,7 @@ export const ReportsTable = ({
   onEditClick,
 }: Props) => {
   const ref = useRef<HTMLDivElement | null>(null);
-  const [isPrintActive, setPrintActive] = useState(false);
+  const [reportToPrintIdx, setReportToPrintIdx] = useState<number | null>(null);
   const { defaultReportBackgroundImageUrl } = useSettingsState();
 
   const classes = useStyles({ background: defaultReportBackgroundImageUrl });
@@ -81,129 +81,136 @@ export const ReportsTable = ({
   const handlePrint = useReactToPrint({
     content: () => ref.current,
     bodyClass: classes.pageBody,
-    documentTitle: 'PlaymakerReport',
-    onAfterPrint: () => setPrintActive(false),
+    documentTitle: 'Report',
+    onAfterPrint: () => setReportToPrintIdx(null),
   }) as () => void;
 
-  function onPrintClick() {
-    setPrintActive(true);
+  function onPrintClick(idx: number) {
+    setReportToPrintIdx(idx);
     setTimeout(() => handlePrint(), 100);
   }
 
   return (
-    <Table
-      page={page}
-      rowsPerPage={rowsPerPage}
-      sortBy={sortBy}
-      order={order}
-      handleChangePage={handleChangePage}
-      handleChangeRowsPerPage={handleChangeRowsPerPage}
-      handleSort={handleSort}
-      total={total}
-      headCells={headCells}
-    >
-      {setStatusLoading && <Loader />}
-      {reports.map((report) => {
-        const {
-          id,
-          player,
-          scout,
-          createdAt,
-          avgRating,
-          maxRatingScore,
-          percentageRating,
-          finalRating,
-          status,
-        } = report;
+    <>
+      <Table
+        page={page}
+        rowsPerPage={rowsPerPage}
+        sortBy={sortBy}
+        order={order}
+        handleChangePage={handleChangePage}
+        handleChangeRowsPerPage={handleChangeRowsPerPage}
+        handleSort={handleSort}
+        total={total}
+        headCells={headCells}
+      >
+        {setStatusLoading && <Loader />}
+        {reports.map((report, idx) => {
+          const {
+            id,
+            player,
+            scout,
+            createdAt,
+            avgRating,
+            maxRatingScore,
+            percentageRating,
+            finalRating,
+            status,
+          } = report;
 
-        return (
-          <StyledTableRow key={id}>
-            <StyledTableCell padding="checkbox">
-              <div className={classes.buttonsContainer}>
-                <Tooltip title="Zobacz szczegóły">
-                  <Link component={RouterLink} to={`/reports/${id}`}>
-                    <IconButton aria-label="go to report">
-                      <SearchIcon />
-                    </IconButton>
-                  </Link>
-                </Tooltip>
-                {onEditClick ? (
-                  <Tooltip title="Edytuj">
+          return (
+            <StyledTableRow key={id}>
+              <StyledTableCell padding="checkbox">
+                <div className={classes.buttonsContainer}>
+                  <Tooltip title="Zobacz szczegóły">
+                    <Link component={RouterLink} to={`/reports/${id}`}>
+                      <IconButton aria-label="go to report">
+                        <SearchIcon />
+                      </IconButton>
+                    </Link>
+                  </Tooltip>
+                  {onEditClick ? (
+                    <Tooltip title="Edytuj">
+                      <IconButton
+                        aria-label="edit report"
+                        onClick={() => onEditClick(report)}
+                        disabled={status === 'closed'}
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                  ) : null}
+                  <Tooltip title="Drukuj">
                     <IconButton
-                      aria-label="edit report"
-                      onClick={() => onEditClick(report)}
-                      disabled={status === 'closed'}
+                      aria-label="print report"
+                      onClick={() => onPrintClick(idx)}
                     >
-                      <EditIcon />
+                      <PrintIcon />
                     </IconButton>
                   </Tooltip>
-                ) : null}
-                <Tooltip title="Drukuj">
-                  <IconButton aria-label="print report" onClick={onPrintClick}>
-                    <PrintIcon />
-                  </IconButton>
-                </Tooltip>
-                {status === 'in-prep' ? (
-                  <Tooltip title="Zamknij raport">
-                    <IconButton
-                      aria-label="close report"
-                      onClick={() => setReportStatus({ id, status: 'closed' })}
-                    >
-                      <CloseIcon />
-                    </IconButton>
-                  </Tooltip>
-                ) : (
-                  <Tooltip title="Otwórz raport">
-                    <IconButton
-                      aria-label="open report"
-                      onClick={() => setReportStatus({ id, status: 'in-prep' })}
-                    >
-                      <OpenIcon />
-                    </IconButton>
-                  </Tooltip>
-                )}
-              </div>
-            </StyledTableCell>
-            <StyledTableCell>
-              <Link
-                component={RouterLink}
-                to={`/players/${player.id}`}
-              >{`${player.firstName} ${player.lastName}`}</Link>
-            </StyledTableCell>
-            <StyledTableCell>
-              {isAdmin ? (
-                <Link component={RouterLink} to={`/users/${scout.id}`}>
-                  {`${scout.firstName} ${scout.lastName}`}
-                </Link>
-              ) : (
-                <>{`${scout.firstName} ${scout.lastName}`}</>
-              )}
-            </StyledTableCell>
-            <StyledTableCell>{formatDate(createdAt, true)}</StyledTableCell>
-            <StyledTableCell>
-              <StatusChip status={status} />
-            </StyledTableCell>
-            <StyledTableCell>{avgRating.toFixed(2)}</StyledTableCell>
-            <StyledTableCell>{maxRatingScore}</StyledTableCell>
-            <StyledTableCell>
-              {`${percentageRating.toFixed(1)}%`}
-            </StyledTableCell>
-            <StyledTableCell>
-              <FinalRatingChip finalRating={finalRating} />
-            </StyledTableCell>
-            {isPrintActive ? (
-              <StyledTableCell>
-                <div className={classes.print}>
-                  <div ref={ref}>
-                    <PrinteableReport report={report} />
-                  </div>
+                  {status === 'in-prep' ? (
+                    <Tooltip title="Zamknij raport">
+                      <IconButton
+                        aria-label="close report"
+                        onClick={() =>
+                          setReportStatus({ id, status: 'closed' })
+                        }
+                      >
+                        <CloseIcon />
+                      </IconButton>
+                    </Tooltip>
+                  ) : (
+                    <Tooltip title="Otwórz raport">
+                      <IconButton
+                        aria-label="open report"
+                        onClick={() =>
+                          setReportStatus({ id, status: 'in-prep' })
+                        }
+                      >
+                        <OpenIcon />
+                      </IconButton>
+                    </Tooltip>
+                  )}
                 </div>
               </StyledTableCell>
-            ) : null}
-          </StyledTableRow>
-        );
-      })}
-    </Table>
+              <StyledTableCell>
+                <Link
+                  component={RouterLink}
+                  to={`/players/${player.id}`}
+                >{`${player.firstName} ${player.lastName}`}</Link>
+              </StyledTableCell>
+              <StyledTableCell>
+                {isAdmin ? (
+                  <Link component={RouterLink} to={`/users/${scout.id}`}>
+                    {`${scout.firstName} ${scout.lastName}`}
+                  </Link>
+                ) : (
+                  <>{`${scout.firstName} ${scout.lastName}`}</>
+                )}
+              </StyledTableCell>
+              <StyledTableCell>{formatDate(createdAt, true)}</StyledTableCell>
+              <StyledTableCell>
+                <StatusChip status={status} />
+              </StyledTableCell>
+              <StyledTableCell>{avgRating.toFixed(2)}</StyledTableCell>
+              <StyledTableCell>{maxRatingScore}</StyledTableCell>
+              <StyledTableCell>
+                {`${percentageRating.toFixed(1)}%`}
+              </StyledTableCell>
+              <StyledTableCell>
+                <FinalRatingChip finalRating={finalRating} />
+              </StyledTableCell>
+            </StyledTableRow>
+          );
+        })}
+      </Table>
+      {reportToPrintIdx !== null ? (
+        <div className={classes.print}>
+          <div ref={ref}>
+            <PrinteableReport report={reports[reportToPrintIdx]} />
+          </div>
+        </div>
+      ) : null}
+    </>
   );
 };
 
