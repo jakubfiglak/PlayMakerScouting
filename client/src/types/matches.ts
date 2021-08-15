703de530bf63128e7a4ca30c662fdb415b38607f
Jakub Figlak <jakub.figlak@gmail.com>
2021-08-15 17:06:55 +0200

    Create basic match types

---
+++ client/src/types/matches.ts
@@ -0,0 +1,32 @@
+import { Club } from './clubs';
+import { Competition } from './reports';
+
+export type Match = {
+  id: string;
+  homeTeam: Pick<Club, 'id' | 'name'>;
+  awayTeam: Pick<Club, 'id' | 'name'>;
+  author: string;
+  competition: Competition;
+  date: string;
+  result?: string;
+  videoURL?: string;
+  isPublic: boolean;
+  createdAt: string;
+  updatedAt: string;
+};
+
+export type MatchBasicInfo = Pick<
+  Match,
+  'id' | 'homeTeam' | 'awayTeam' | 'competition' | 'date'
+>;
+
+export type MatchDTO = Pick<
+  Match,
+  'competition' | 'date' | 'result' | 'videoURL'
+> & { homeTeam: string; awayTeam: string };
+
+export type MatchFilterData = {
+  club: string;
+  afterDate: string;
+  beforeDate: string;
+};
