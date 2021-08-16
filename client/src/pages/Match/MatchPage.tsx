803071f71f7dc89ee989e9d59e880da9ea8cbd04
Jakub Figlak <jakub.figlak@gmail.com>
2021-08-16 13:32:28 +0200

    Display notes table at matches page

--- client/src/pages/Match/MatchPage.tsx
+++ client/src/pages/Match/MatchPage.tsx
@@ -2,15 +2,14 @@ import { Link as RouterLink, useParams } from 'react-router-dom';
 // MUI components
 import { Button, Typography, makeStyles, Theme } from '@material-ui/core';
 // Custom components
-// import { PlayerDetails } from './PlayerDetails';
-import { ReportsTable } from '../Reports/ReportsTable';
-import { ReportsTableRow } from '../Reports/ReportsTableRow';
+import { NotesTable } from '../Notes/NotesTable';
+import { NotesTableRow } from '../Notes/NotesTableRow';
 import { MainTemplate } from '../../templates/MainTemplate';
 import { PageHeading } from '../../components/PageHeading';
 import { Loader } from '../../components/Loader';
 // Hooks
 import { useMatch } from '../../hooks/matches';
-import { usePlayersReports } from '../../hooks/reports';
+import { useMatchesNotes } from '../../hooks/notes';
 import { useTable } from '../../hooks/useTable';
 import { MatchDetails } from './MatchDetails';
 
@@ -26,21 +25,21 @@ export const MatchPage = () => {
     handleChangePage,
     handleChangeRowsPerPage,
     handleSort,
-  } = useTable('playersReportsTable');
+  } = useTable('matchesNotesTable');
 
   const { id } = params;
 
   const { data: match, isLoading: matchLoading } = useMatch(id);
 
-  // const { data: reports } = usePlayersReports({
-  //   playerId: id,
-  //   page: page + 1,
-  //   order,
-  //   limit: rowsPerPage,
-  //   sort: sortBy,
-  // });
+  const { data: notes, isLoading: notesLoading } = useMatchesNotes({
+    matchId: id,
+    page: page + 1,
+    order,
+    limit: rowsPerPage,
+    sort: sortBy,
+  });
 
-  const isLoading = matchLoading;
+  const isLoading = matchLoading || notesLoading;
 
   return (
     <MainTemplate>
@@ -58,30 +57,32 @@ export const MatchPage = () => {
         <PageHeading title="Szczegóły meczu" />
       </div>
       {match ? <MatchDetails match={match} /> : null}
-      <Typography
-        variant="h6"
-        component="h3"
-        align="center"
-        className={classes.title}
-      >
-        Notatki
-      </Typography>
-      {/* <ReportsTable
-        page={page}
-        rowsPerPage={rowsPerPage}
-        sortBy={sortBy}
-        order={order}
-        handleChangePage={handleChangePage}
-        handleChangeRowsPerPage={handleChangeRowsPerPage}
-        handleSort={handleSort}
-        total={reports?.totalDocs || 0}
-      >
-        {reports
-          ? reports.docs.map((report) => (
-              <ReportsTableRow key={report.id} report={report} />
-            ))
-          : null}
-      </ReportsTable> */}
+      <section>
+        <Typography
+          variant="h6"
+          component="h3"
+          align="center"
+          className={classes.title}
+        >
+          Notatki
+        </Typography>
+        <NotesTable
+          page={page}
+          rowsPerPage={rowsPerPage}
+          sortBy={sortBy}
+          order={order}
+          handleChangePage={handleChangePage}
+          handleChangeRowsPerPage={handleChangeRowsPerPage}
+          handleSort={handleSort}
+          total={notes?.totalDocs || 0}
+        >
+          {notes
+            ? notes.docs.map((note) => (
+                <NotesTableRow key={note.id} note={note} />
+              ))
+            : null}
+        </NotesTable>
+      </section>
     </MainTemplate>
   );
 };
